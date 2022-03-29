eb4e01c56cbdac5059ad814af0c65e1ceec0fbac
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-29 11:38:36 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lis = new List<int>();
+            lis.Add(29);
+            lis.Add(30);
+            lis.Add(26);
+            lis.Add(10);
+            foreach(var x in lis)
+            {
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine(x);
+
+                }
+            }
+
+        }
+    }
+}
